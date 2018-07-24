fded1e5eb08676966f85bf12e55da86a6587fd59
stone <674656681@qq.com>
2018-07-24 10:38:32 +0800

    canvas
    Signed-off-by: stone <674656681@qq.com>

--- routes/index.js
+++ routes/index.js
@@ -29,7 +29,21 @@ router.get('/', function (req, res, next) {
 var Canvas = require('canvas');
 
 router.get('/canvas', function (req, res, next) {
- 
+  var Image = Canvas.Image,
+    canvas = new Canvas(200, 200),
+    ctx = canvas.getContext('2d');
+
+  ctx.font = '30px Impact';
+  ctx.rotate(.1);
+  ctx.fillText("Awesome!", 50, 100);
+
+  var te = ctx.measureText('Awesome!');
+  ctx.strokeStyle = 'rgba(0,0,0,0.5)';
+  ctx.beginPath();
+  ctx.lineTo(50, 102);
+  ctx.lineTo(50 + te.width, 102);
+  ctx.stroke();
+  res.send('<img src="' + canvas.toDataURL() + '" />')
 
 });
 
