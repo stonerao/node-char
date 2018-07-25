b2bf03e03a8e7723792c2ff5ab2ed774397f3c4f
stone <674656681@qq.com>
2018-07-25 18:50:53 +0800

    生成PDF
    Signed-off-by: stone <674656681@qq.com>

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var express = require('express');
 const superagent = require('superagent');
 //nodejs里一个非常方便的客户端请求代理模块
 const cheerio = require('cheerio');
+var webPage = require('webpage');
+//  
 var router = express.Router();
+
+
+/* 
+
 require('chromedriver')
 var webdriver = require('selenium-webdriver'),
   By = webdriver.By,
@@ -11,7 +17,7 @@ var webdriver = require('selenium-webdriver'),
 
 var driver = new webdriver.Builder()
   .forBrowser('chrome')
-  .build();
+  .build(); */
 
 
 const fs = require('fs');
@@ -26,74 +32,127 @@ router.get('/', function (req, res, next) {
 });
 
 
-var Canvas = require('canvas');
-var node_echarts = require('node-echarts');
-
+var d3 = require('d3');
+var jsdom = require('jsdom');
+const {
+  JSDOM
+} = jsdom;
+var phantom = require('phantom');
+var num = 1;
+//生成PDF报表
 router.get('/canvas', function (req, res, next) {
-  /* node_echarts({
-    width: 500, // Image width, type is number.
-    height: 500, // Image height, type is number.
-    option: {
-      xAxis: {
-        type: "category",
-        data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
-      },
-      yAxis: {
-        type: "value"
-      },
-      series: [{
-        animation: false,
-        data: [820, 932, 901, 934, 1290, 1330, 1320],
-        type: "line",
-        animationDelay: function (idx) {
-          return 0;
-        }
-      }]
-    }, // Echarts configuration, type is Object.
-    //If the path  is not set, return the Buffer of image.
-    path:__dirname + '/demo.png', // Path is filepath of the image which will be created.
-  }) */
-  var Image = Canvas.Image,
-    canvas = new Canvas(200, 200),
-    ctx = canvas.getContext('2d');
+  phantom.create().then(function (ph) {
+    ph.createPage().then(function (page) {
+      page.open("http://localhost:3000/chart").then(function (status) {
+        page.property('viewportSize', {
+          width: 595,
+          height: 500
+        });
+        page.property('paperSize', {
+          format: 'A4',
+        });
+        var url = __dirname + '/' + (+new Date) + 'pdf.pdf'
+        page.render(url).then(function () {
+          num++;
+          console.log('Page rendered');
+          ph.exit();
+          res.send(` 
+            <script>
+             
+            </script>
+          `)
+        });
+      });
+    });
+  });
 
-  ctx.font = '30px Impact';
-  ctx.rotate(.1);
-  ctx.fillText("Awesome!", 50, 100);
 
-  var te = ctx.measureText('Awesome!');
-  ctx.strokeStyle = 'rgba(0,0,0,0.5)';
-  ctx.beginPath();
-  ctx.lineTo(50, 102);
-  ctx.lineTo(50 + te.width, 102);
-  ctx.stroke();
-  res.send('<img src="' + canvas.toDataURL() + '" />')
+
+
+
 
 });
-
+//抓取该页面
 router.get('/chart', function (req, res, next) {
-  driver.get("http://localhost:3000/")
+  const dom = new JSDOM(`<!DOCTYPE html>
+  <body>
+  <h1>我是PDF1</h1>
+  <svg id="svg"></svg>
+  <br> 
+  <h1>我是PDF2</h1>
+  <svg class="svg1"></svg>
+  
+  <br>
+  <h1>我是PDF3</h1>
+  <svg class="svg2"></svg>
+  
+  <br>
+  <h1>我是PDF4</h1>
+  <svg class="svg3"></svg>
+  
+  <br>
+  <h1>我是PDF4</h1>
+  <svg class="svg4"></svg>
+  <h1 style="position:absolute;bottom:0">我是中文，我不会乱码</h1>
+  </body>
+
+
+`);
+  // console.log(dom.window.document.querySelector("p").textContent);
+  var rectHeight = 25;
+  var svg = d3.select(dom.window.document.querySelector("#svg")).attr('width', 595)
+    .attr('height', 600);
+  var svg1 = d3.select(dom.window.document.querySelector(".svg1")).attr('width', 595).attr('height', 600);
+  var svg2 = d3.select(dom.window.document.querySelector(".svg2")).attr('width', 595).attr('height', 600);
+  var svg3 = d3.select(dom.window.document.querySelector(".svg3")).attr('width', 595).attr('height', 600);
+  var svg4 = d3.select(dom.window.document.querySelector(".svg4")).attr('width', 595).attr('height', 600);
+  var dataset = [250, 210, 170, 130, 90];
+  var dataset1 = [1, 500, 170, 230, 90, 333, 521];
+  svg.selectAll("rect")
+    .data(dataset)
+    .enter()
+    .append("rect")
+    .attr("x", 20)
+    .attr("y", function (d, i) {
+      return i * rectHeight;
+    })
+    .attr("width", function (d) {
+      return d;
+    })
+    .attr("height", rectHeight - 2)
+    .attr("fill", "steelblue");
+    function svgs(SVG){
+      SVG.selectAll("rect")
+      .data(dataset1)
+      .enter()
+      .append("rect")
+      .attr("x", 20)
+      .attr("y", function (d, i) {
+        return i * rectHeight;
+      })
+      .attr("width", function (d) {
+        return d;
+      })
+      .attr("height", rectHeight - 2)
+      .attr("fill", "steelblue");
+    } 
+    svgs(svg1)
+    svgs(svg2)
+    svgs(svg3)
+    svgs(svg4)
+  res.send(dom.window.document.body.innerHTML)
+  /* driver.get("http://localhost:3000/")
   driver.sleep(1 * 1000).then(function (rr) {
-    // res.send(JSON.stringify(driver.findElement(By.id('cc'))).toString())
-    // res.send(driver.findElement(By.id('cc')))
-    // var body = driver.findElement()
-    /*   By.id("canvas").getAttribute("style").then(b => {
-
-
-      }) */
-    /*  driver.findElement(By.id('cc')).getPageSource().then(b => {
-      
-     }); */
+     
     driver.getPageSource().then(function (souce) {
-      let $ = cheerio.load(souce);
-      // console.log($("cc"))
+      let $ = cheerio.load(souce); 
       res.send($("#cc").html())
     })
 
 
     // driver.quit(); //关闭浏览器
   })
-
+ */
 
 
 });
